11bca47638e7dd232df458ddfda4e67721b55d05
Julk1 <babailova.j.b@gmail.com>
2023-05-28 15:06:32 +0200

    lesson38

--- react11/src/App.js
+++ react11/src/App.js
@@ -2,11 +2,25 @@
 import { Contacts } from './components/Contacts';
 import { MyPhoto } from './components/MyPhoto';
 import { Experience } from './components/Experience';
+import { Counter } from './components/Counter';
+import WorkExperienceDetails from './components/WorkExperienceDetails';
+import AddExperienceForm from './components/AddExperienceForm';
+
+
+
 import '../src/styles/style copy.css'
 
 const photoLink = './my-photo.jpg';
 
 function App() {
+
+    const [workExperienceItems, setWorkExperienceItems] = useState([]);
+
+    function addWorkExperience(newItem) {
+    setWorkExperienceItems([...workExperienceItems, newItem]);
+
+  }
+
   return <div className='main-theme'>
          <div className='grid-container'>
             <div className='grid-item-1'>
@@ -18,13 +32,22 @@ function App() {
             <Contacts/>
             </div>
 
-         <div className='grid-item-2'>
+       
+
            <Experience/>
 
+           {workExperienceItems.map(item => (
+          <WorkExperienceItem key={item.name} {...item} />
+        ))}
+        <WorkExperienceForm onAddItem={addWorkExperience} />
+
+
+    <Counter/>
+  
     
       </div>
      </div>
-    </div>
+   
   
 }
 
--- react11/src/components/Experience.js
+++ react11/src/components/Experience.js
@@ -1,25 +1,38 @@
-export function Experience() {
+import React, { useState } from "react";
+import WorkExperienceDetails from "./WorkExperienceDetails";
+
+ export function Experience() {
+
+  const [workExperience] = useState([
+    {
+      title: "BIM Engineer",
+      name: "Strabag",
+      description: "Work with Estimating and Proposal departments to develop BIM modeling for estimates and proposal submittals.",
+      link: "https://www.strabag.com/"
+
+    },
+    
+ ]);
+
+
     return (
             
 
 <div className="work">
 <h2 className="header">Work experience</h2>
-<div className="main-text">
-
-    <span className= "bold-text">Name of employer: </span> Strabag <br/>
-    <span className= "bold-text">Dates of employer: </span>06/2021<br/>
-    <span className= "bold-text">Job title:</span> BIM Engineer <br/>
-    <span className= "bold-text">Project/Role description:</span> Work with Estimating and Proposal departments to develop BIM modeling for estimates and proposal submittals. <br/>
-  
+<div className="work-descr">
 
 
-   <p><span className= "bold-text">Name of employer:</span> Wiener Linien<br/>
-      <span className= "bold-text">Dates of employment:</span> 05/2018 - 09/2019<br/>
-      <span className= "bold-text">Job title:</span> Research assistant<br/>
-      <span className= "bold-text">Project/Role description:</span> Participate in Sales and Marketing process for design build work.<br/></p>
-</div>
-</div>
-
-
-);
-    }
+        {Experience.map((item, index) => (
+          <WorkExperienceDetails
+            key={index}
+            title={item.title}
+            name={item.name}
+            description={item.description}
+            link={item.link}
+          />
+          ))}
+        </div>
+      </div>
+    );
+  }
